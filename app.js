9e7c7792cf035aab70ad95cc4a6ccd331b8856c2
Tessa Thornton <tessathornton@gmail.com>
2013-05-12 17:28:38 -0400

    functioning local list of names

---
+++ app.js
@@ -0,0 +1,56 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express'), 
+    routes = require('./routes'),
+    user = require('./routes/user') , 
+    http = require('http'),
+    path = require('path'),
+    fs = require("fs");
+    request = require('request');
+
+var appConfig = JSON.parse(fs.readFileSync("config.json"));
+var app = express();
+
+var headers = {
+  'Authorization': 'Basic ' + new Buffer(appConfig.username + ':' + appConfig.password).toString('base64'),
+  'Content-Type': 'application/xml',
+  'Accept': 'application/xml'
+};
+
+
+
+// all environments
+app.set('port', process.env.PORT || 1234);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', function(req,res){
+  res.sendfile('index.html');
+});
+
+app.get('/users', function(req, res){
+  request.get('https://thephuse.harvestapp.com/people/', {
+    headers: headers
+  }, function(error, response, body) {
+    res.send(body);
+  })
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
