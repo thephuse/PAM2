d205b3cda7354a16ae34eb60bd8c879187281822
Tessa Thornton <tessathornton@gmail.com>
2013-05-13 15:11:50 -0400

    change default port

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ app.get('/users/:id/:start/:end', function(req, res) {
   })
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 1234;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
