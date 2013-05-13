0ef9156517a281578e2816a56a46013280619d5a
Tessa Thornton <tessathornton@gmail.com>
2013-05-13 14:53:02 -0400

    copy over code from heroku test app

--- app.js
+++ app.js
@@ -1,17 +1,8 @@
-
-/**
- * Module dependencies.
- */
-
-var express = require('express'),  
-    http = require('http'),
-    path = require('path'),
-    fs = require("fs");
-    request = require('request');
-
-var appConfig = JSON.parse(fs.readFileSync("config.json"));
-
+var express = require("express");
 var app = express();
+var request = require('request');
+var fs = require("fs");
+app.use(express.logger());
 
 app.configure(function(){
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
@@ -19,52 +10,47 @@ app.configure(function(){
   app.use(app.router);
 });
 
+var appConfig = JSON.parse(fs.readFileSync("config.json"));
+
 var headers = {
   'Authorization': 'Basic ' + new Buffer(appConfig.username + ':' + appConfig.password).toString('base64'),
   'Content-Type': 'application/xml',
   'Accept': 'application/xml'
 };
 
-// all environments
-app.set('port', process.env.PORT || 1234);
-
 app.get('/', function(req,res){
   res.sendfile('index.html');
 });
 
-// app.get('/users/:id/:start/:end', function(req, res) {
-//   request.get('https://thephuse.harvestapp.com/people/' + req.params.id + '/entries?from=' + req.params.start + '&to=' + req.params.end, {
-//     headers: headers
-//   }, function(error, response, body){
-//     res.send(body);
-//     if (error) {
-//       console.log(error);
-//     }
-//   })
-// });
 
-// app.get('/users/:id/billable/:start/:end', function(req, res) {
-//   request.get('https://thephuse.harvestapp.com/people/' + req.params.id + '/entries?from=' + req.params.start + '&to=' + req.params.end + '&billable=yes', {
-//     headers: headers
-//   }, function(error, response, body){
-//     res.send(body);
-//     if (error) {
-//       console.log(error);
-//     }
-//   })
-// });
-
-app.get('/users', function(req, res){
+app.get('/users', function(req, res) {
   request.get('https://thephuse.harvestapp.com/people/', {
     headers: headers
-  }, function(error, response, body) {
-    if (!error && response.statusCode == 200) {
+  },function(error, response, body){
     res.send(body);
-  }
+  });
+});
+
+app.get('/users/:id/billable/:start/:end', function(req, res) {
+  request.get('https://thephuse.harvestapp.com/people/' + req.params.id + '/entries?from=' + req.params.start + '&to=' + req.params.end + '&billable=yes', {
+    headers: headers
+  }, function(error, response, body){
+    res.send(body);
+    if (error) {
+      console.log(error);
+    }
   })
 });
 
-app.listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+app.get('/users/:id/:start/:end', function(req, res) {
+  request.get('https://thephuse.harvestapp.com/people/' + req.params.id + '/entries?from=' + req.params.start + '&to=' + req.params.end, {
+    headers: headers
+  }, function(error, response, body){
+    res.send(body);
+  })
 });
 
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
