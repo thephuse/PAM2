75ea9849ff6ada871e902e9cf78595220f4961ce
Tessa Thornton <tessathornton@gmail.com>
2013-09-20 16:19:29 -0400

    add jshint

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,12 +18,31 @@ module.exports = function(grunt) {
           '<%= dir.local %>/css/main.css': '<%= dir.local %>/stylus/main.styl'
         }
       }
+    },
+
+    jshint: {
+      files: ['gruntfile.js', '<%= dir.local %>/models/**/*.js', '<%= dir.local %>/collections/**/*.js', '<%= dir.local %>/views/**/*.js'],
+      options: {
+          // more options here if you want to override JSHint defaults
+        globals: {
+          jQuery: true,
+          console: true,
+          module: true
+        }
+      }
+    },
+
+    watch: {
+      files: ['<%= dir.local %>/stylus/{,*/}*.styl'],
+      tasks: ['stylus']
     }
 
   });
 
   grunt.loadNpmTasks('grunt-contrib-stylus');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
 
-  grunt.registerTask('default', ['stylus']);
+  grunt.registerTask('default', ['stylus', 'jshint']);
 
 };
