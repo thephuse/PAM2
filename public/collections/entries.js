90e82cac14bba81b6f429b14e369bdce8f2840e9
Tessa Thornton <tessathornton@gmail.com>
2013-09-20 16:19:44 -0400

    update js from linting

--- public/collections/entries.js
+++ public/collections/entries.js
@@ -1,18 +1,18 @@
-var app = app || {}; 
+var app = app || {};
 
 var EntryList = Backbone.Collection.extend ({
   model: app.Entry,
   parse: function (data) {
     var parsed = [];
     $(data).find('day-entry').each(function (index) {
-      var hours = $(this).find("hours").text();           
+      var hours = $(this).find("hours").text();
       parsed.push({hours: hours});
-    })
+    });
     return parsed;
   },
   fetch: function (options) {
     options = options || {};
     options.dataType = "xml";
     Backbone.Collection.prototype.fetch.call(this, options);
-  }    
+  }
 });
